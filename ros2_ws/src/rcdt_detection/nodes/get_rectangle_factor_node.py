2950668b937906abe93f62b7214a1394639ef38f
Wim-Peter Dirks <wim-peter.dirks@ordina.nl>
2024-12-13 11:10:47 +0100

    feat: simplify GetRectangleFactor-service
    Signed-off-by: Wim-Peter Dirks <wim-peter.dirks@ordina.nl>

--- ros2_ws/src/rcdt_detection/nodes/get_rectangle_factor_node.py
+++ ros2_ws/src/rcdt_detection/nodes/get_rectangle_factor_node.py
@@ -11,8 +11,6 @@ from rclpy import logging
 from rclpy.node import Node
 
 from rcdt_detection_msgs.srv import GetRectangleFactor
-from rcdt_detection.image_manipulation import three_to_single_channel
-from rcdt_utilities.cv_utils import ros_image_to_cv2_image, cv2_image_to_ros_image
 
 ros_logger = logging.get_logger(__name__)
 
@@ -25,36 +23,18 @@ class GetRectangleFactorNode(Node):
     def callback(
         self, request: GetRectangleFactor.Request, response: GetRectangleFactor.Response
     ) -> GetRectangleFactor.Response:
-        """Determine rectangle factor of largest contour in image.
+        """Determine rectangle factor (100 / mask_area * bounding_box_area).
 
-        The rectangle factor is: 100 / contour_area * contour_area. The contour
-        area is based on the closest-fitting rectangle around the contour.
-
-        Returns success=False if no contours were found.
+        Always returns success=True.
 
         """
-        cv2_image = ros_image_to_cv2_image(request.image)
-        single_channel = three_to_single_channel(cv2_image)
+        ros_logger.info(f"{request.mask_contour}")
 
-        contours, _ = cv2.findContours(
-            single_channel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
-        if not contours:
-            ros_logger.error("No contours found.")
-            response.success = False
-            return response
-        countour_areas = [cv2.contourArea(contour) for contour in contours]
-        largest_contour = contours[countour_areas.index(max(countour_areas))]
-
-        area = cv2.contourArea(largest_contour)
-        (x, y), (w, h), angle = cv2.minAreaRect(largest_contour)
-        rectangle_factor = 100 / area * (w * h)
+        mask_area = cv2.contourArea(request.mask_contour)
+        bounding_box_area = cv2.contourArea(request.bounding_box_contour)
+        rectangle_factor = 100 / mask_area * bounding_box_area
         ros_logger.info(f"Rectangle factor: {rectangle_factor}")
 
-        box = np.int32(cv2.boxPoints(((x, y), (w, h), angle)))
-        marked_image = cv2.drawContours(cv2_image, [box], 0, (255, 0, 255), 2)
-
-        response.image = cv2_image_to_ros_image(marked_image)
         response.factor = rectangle_factor
         response.success = True
         return response
