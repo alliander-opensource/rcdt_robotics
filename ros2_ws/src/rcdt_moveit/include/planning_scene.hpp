7b734008918df9a53a4d33d8edabecb6347aa8f3
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-01-10 12:57:34 +0000

    Add service to add and remove collision objects.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_moveit/include/planning_scene.hpp
+++ ros2_ws/src/rcdt_moveit/include/planning_scene.hpp
@@ -1,5 +1,11 @@
 #include <moveit/planning_scene_monitor/planning_scene_monitor.hpp>
+#include <rcdt_utilities_msgs/srv/add_object.hpp>
 #include <rclcpp/node.hpp>
+#include <std_srvs/srv/detail/trigger__struct.hpp>
+#include <std_srvs/srv/trigger.hpp>
+
+typedef rcdt_utilities_msgs::srv::AddObject AddObject;
+typedef std_srvs::srv::Trigger Trigger;
 
 class PlanningScene {
 public:
@@ -7,5 +13,16 @@ public:
   planning_scene_monitor::PlanningSceneMonitorPtr get_planning_scene();
 
 private:
+  std::shared_ptr<rclcpp::Node> node;
   planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor;
+
+  std::map<std::string, int> shapes = {
+      {"BOX", 1}, {"SPHERE", 2}, {"CYLINDER", 3}, {"CONE", 4}};
+  rclcpp::Service<AddObject>::SharedPtr add_object_service;
+  void add_object(const std::shared_ptr<AddObject::Request> request,
+                  std::shared_ptr<AddObject::Response> response);
+
+  rclcpp::Service<Trigger>::SharedPtr clear_objects_service;
+  void clear_objects(const std::shared_ptr<Trigger::Request> request,
+                     std::shared_ptr<Trigger::Response> response);
 };
--- ros2_ws/src/rcdt_moveit/src/planning_scene.cpp
+++ ros2_ws/src/rcdt_moveit/src/planning_scene.cpp
@@ -1,8 +1,15 @@
 #include "planning_scene.hpp"
+#include <geometry_msgs/msg/detail/pose_stamped__struct.hpp>
 #include <memory>
-#include <moveit/planning_scene_monitor/planning_scene_monitor.hpp>
+#include <moveit/planning_interface/planning_interface.hpp>
+#include <rclcpp/logging.hpp>
+#include <stdexcept>
 
-void PlanningScene::initialize(std::shared_ptr<rclcpp::Node> node) {
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+void PlanningScene::initialize(std::shared_ptr<rclcpp::Node> node_) {
+  node = node_;
   robot_model_loader::RobotModelLoader robot_model_loader(node);
   planning_scene_monitor =
       std::make_shared<planning_scene_monitor::PlanningSceneMonitor>(
@@ -11,9 +18,53 @@ void PlanningScene::initialize(std::shared_ptr<rclcpp::Node> node) {
   planning_scene_monitor->startStateMonitor();
   planning_scene_monitor->startSceneMonitor();
   planning_scene_monitor->providePlanningSceneService();
+
+  add_object_service = node->create_service<AddObject>(
+      "~/add_object", std::bind(&PlanningScene::add_object, this, _1, _2));
+  clear_objects_service = node->create_service<Trigger>(
+      "~/clear_objects",
+      std::bind(&PlanningScene::clear_objects, this, _1, _2));
 };
 
 planning_scene_monitor::PlanningSceneMonitorPtr
 PlanningScene::get_planning_scene() {
   return planning_scene_monitor;
+};
+
+void PlanningScene::add_object(
+    const std::shared_ptr<AddObject::Request> request,
+    std::shared_ptr<AddObject::Response> response) {
+  moveit_msgs::msg::CollisionObject collision_object;
+  collision_object.header = request->pose.header;
+
+  shape_msgs::msg::SolidPrimitive solid_primitive;
+  try {
+    solid_primitive.type = shapes.at(request->shape);
+  } catch (std::out_of_range) {
+    RCLCPP_ERROR(node->get_logger(), "Shape %s is not a valid option.}. Exit.",
+                 request->shape.c_str());
+    return;
+  }
+
+  solid_primitive.dimensions = {request->d1, request->d2, request->d3};
+  collision_object.primitives.push_back(solid_primitive);
+  collision_object.primitive_poses.push_back(request->pose.pose);
+  collision_object.operation = collision_object.ADD;
+  {
+    auto planning_scene =
+        planning_scene_monitor::LockedPlanningSceneRW(planning_scene_monitor);
+    planning_scene->processCollisionObjectMsg(collision_object);
+  }
+  response->success = true;
+};
+
+void PlanningScene::clear_objects(
+    const std::shared_ptr<Trigger::Request> request,
+    std::shared_ptr<Trigger::Response> response) {
+  {
+    auto planning_scene =
+        planning_scene_monitor::LockedPlanningSceneRW(planning_scene_monitor);
+    planning_scene->removeAllCollisionObjects();
+  }
+  response->success = true;
 };
