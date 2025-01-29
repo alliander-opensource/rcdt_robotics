f65fdf9497ecc92192ab78189570baf213ca4eea
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-01-29 12:06:07 +0000

    Add launch order.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

---
+++ ros2_ws/src/rcdt_utilities/src_py/wait_for_topic.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import rclpy
+import time
+from rclpy import logging
+from rclpy.node import Node
+from rclpy.wait_for_message import wait_for_message
+from sensor_msgs.msg import JointState
+
+ros_logger = logging.get_logger(__name__)
+
+MSG_TYPES = {"JointState": JointState}
+WAIT = 3
+
+
+class WaitForTopic(Node):
+    def __init__(self) -> bool:
+        super().__init__("wait_for_topic")
+        self.declare_parameter("topic", "")
+        self.declare_parameter("msg_type", "")
+
+        topic = self.get_parameter("topic").get_parameter_value().string_value
+        if topic == "":
+            self.error("Empty topic is not supported.")
+
+        msg_type_str = self.get_parameter("msg_type").get_parameter_value().string_value
+        msg_type = MSG_TYPES.get(msg_type_str)
+        if msg_type is None:
+            self.error(f"msg_type '{msg_type_str}' is not supported.")
+
+        success = False
+        while not success:
+            success, _ = wait_for_message(msg_type, self, topic, time_to_wait=WAIT)
+            if not success:
+                self.get_logger().warn(
+                    f"No message received on topic '{topic}' of type '{msg_type_str}'. Continue waiting..."
+                )
+
+    def error(self, msg: str) -> None:
+        self.get_logger().error(msg)
+        while rclpy.ok:
+            self.get_logger().error("Please restart.")
+            time.sleep(WAIT)
+
+
+def main(args: str = None) -> None:
+    rclpy.init(args=args)
+    WaitForTopic()
+
+
+if __name__ == "__main__":
+    main()
