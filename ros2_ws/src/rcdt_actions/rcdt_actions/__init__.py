e2a14b28465fede7874932ffc1b13fdf96651d1e
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-02-10 11:06:24 +0000

    Fix ruff format.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_actions/rcdt_actions/__init__.py
+++ ros2_ws/src/rcdt_actions/rcdt_actions/__init__.py
@@ -2,4 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from rcdt_actions.moveit.moveit import move_to_configuration, move_hand_to_pose, tranform_goal_pose
+from rcdt_actions.moveit.moveit import (
+    move_to_configuration,
+    move_hand_to_pose,
+    tranform_goal_pose,
+)
