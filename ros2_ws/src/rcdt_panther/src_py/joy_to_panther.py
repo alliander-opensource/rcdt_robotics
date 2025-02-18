6464537d3616090b2f1037248ed2128a3e2a5659
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-02-18 09:04:54 +0000

    Fix ruff.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_panther/src_py/joy_to_panther.py
+++ ros2_ws/src/rcdt_panther/src_py/joy_to_panther.py
@@ -5,9 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import rclpy
+from rcdt_utilities.launch_utils import spin_node
 from rclpy import logging
 from rclpy.node import Node
-from rcdt_utilities.launch_utils import spin_node
 from sensor_msgs.msg import Joy
 
 ros_logger = logging.get_logger(__name__)
