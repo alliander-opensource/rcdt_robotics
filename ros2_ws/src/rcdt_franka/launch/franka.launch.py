184be6d459f19521973efa4a5e4ded1a19d95a3e
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-02-18 09:03:26 +0000

    Fix rviz when not using moveit.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_franka/launch/franka.launch.py
+++ ros2_ws/src/rcdt_franka/launch/franka.launch.py
@@ -79,6 +79,7 @@ def launch_setup(context: LaunchContext) -> None:
     rviz = IncludeLaunchDescription(
         get_file_path("rcdt_utilities", ["launch"], "rviz.launch.py"),
         launch_arguments={
+            "rviz_display_config": "planning.rviz",
             "robot_name": "fr3",
             "moveit_package_name": "rcdt_franka_moveit_config",
         }.items(),
