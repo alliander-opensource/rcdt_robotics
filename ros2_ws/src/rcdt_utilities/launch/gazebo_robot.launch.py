7ec0ad52fbc807d81acb47084833434d32ba69ff
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-04-02 07:54:05 +0000

    Fix offsets due to husarion's new frame definitions.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_utilities/launch/gazebo_robot.launch.py
+++ ros2_ws/src/rcdt_utilities/launch/gazebo_robot.launch.py
@@ -15,6 +15,10 @@ namespace_arg = LaunchArgument("namespace", "")
 use_realsense_arg = LaunchArgument("realsense", False, [True, False])
 use_velodyne_arg = LaunchArgument("velodyne", False, [True, False])
 
+x_arg = LaunchArgument("x", 0)
+y_arg = LaunchArgument("y", 0)
+z_arg = LaunchArgument("z", 0)
+
 
 def launch_setup(context: LaunchContext) -> List:
     load_gazebo_ui = load_gazebo_ui_arg.value(context)
@@ -22,6 +26,10 @@ def launch_setup(context: LaunchContext) -> List:
     namespace = namespace_arg.value(context)
     use_realsense = use_realsense_arg.value(context)
     use_velodyne = use_velodyne_arg.value(context)
+
+    x = x_arg.value(context)
+    y = y_arg.value(context)
+    z = z_arg.value(context)
 
     sdf_file = get_file_path("rcdt_gz_worlds", ["worlds"], world)
     gz_args = f" -r {sdf_file}"
@@ -32,10 +40,15 @@ def launch_setup(context: LaunchContext) -> List:
         launch_arguments={"gz_args": gz_args}.items(),
     )
 
+    arguments = []
+    arguments.extend(["-topic", f"{namespace}/robot_description"])
+    arguments.extend(["-x", str(x)])
+    arguments.extend(["-y", str(y)])
+    arguments.extend(["-z", str(z)])
     spawn_robot = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=["-topic", f"{namespace}/robot_description"],
+        arguments=arguments,
         output="screen",
     )
 
@@ -78,6 +91,9 @@ def generate_launch_description() -> LaunchDescription:
             namespace_arg.declaration,
             use_realsense_arg.declaration,
             use_velodyne_arg.declaration,
+            x_arg.declaration,
+            y_arg.declaration,
+            z_arg.declaration,
             OpaqueFunction(function=launch_setup),
         ]
     )
