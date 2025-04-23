66471ff2e857494acdbe4a63edf91c80261f6d70
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-04-23 13:39:25 +0000

    Add integration test for panther.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

---
+++ ros2_ws/src/rcdt_panther/launch/core.launch.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from launch import LaunchContext, LaunchDescription
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch_ros.actions import Node, SetParameter
+from rcdt_utilities.launch_utils import (
+    LaunchArgument,
+    get_file_path,
+    get_robot_description,
+)
+
+use_sim_arg = LaunchArgument("simulation", True, [True, False])
+load_gazebo_ui_arg = LaunchArgument("load_gazebo_ui", False, [True, False])
+use_velodyne_arg = LaunchArgument("velodyne", False, [True, False])
+
+
+def launch_setup(context: LaunchContext) -> None:
+    use_sim = use_sim_arg.value(context)
+    load_gazebo_ui = load_gazebo_ui_arg.value(context)
+    use_velodyne = use_velodyne_arg.value(context)
+
+    xacro_path = get_file_path("rcdt_panther", ["urdf"], "panther.urdf.xacro")
+    components_path = get_file_path("rcdt_panther", ["config"], "components.yaml")
+    xacro_arguments = {"use_sim": "true", "components_config_path": components_path}
+    xacro_arguments["load_velodyne"] = "true" if use_velodyne else "false"
+    robot_description = get_robot_description(xacro_path, xacro_arguments)
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace="panther",
+        parameters=[robot_description, {"frame_prefix": "panther/"}],
+    )
+
+    robot = IncludeLaunchDescription(
+        get_file_path("rcdt_utilities", ["launch"], "gazebo_robot.launch.py"),
+        launch_arguments={
+            "world": "walls.sdf",
+            "namespace": "panther",
+            "velodyne": str(use_velodyne),
+            "load_gazebo_ui": str(load_gazebo_ui),
+        }.items(),
+    )
+
+    controllers = IncludeLaunchDescription(
+        get_file_path("rcdt_panther", ["launch"], "controllers.launch.py"),
+    )
+
+    return [
+        SetParameter(name="use_sim_time", value=use_sim),
+        robot_state_publisher,
+        robot,
+        controllers,
+    ]
+
+
+def generate_launch_description() -> LaunchDescription:
+    return LaunchDescription(
+        [
+            use_sim_arg.declaration,
+            load_gazebo_ui_arg.declaration,
+            use_velodyne_arg.declaration,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
