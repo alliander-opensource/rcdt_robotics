f9a97e9fef407f72730041471282f5d85b9e976e
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-05-12 10:18:30 +0000

    Rename to be consistent with rcdt_panther.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_franka/launch/core.launch.py
+++ ros2_ws/src/rcdt_franka/launch/core.launch.py
@@ -13,8 +13,7 @@ from rcdt_utilities.launch_utils import (
 )
 
 use_sim_arg = LaunchArgument("simulation", True, [True, False])
-start_robot_arg = LaunchArgument("start_robot", True, [True, False])
-base_link_arg = LaunchArgument("base_link", "world")
+parent_arg = LaunchArgument("parent", "world", ["world", "panther"])
 load_gazebo_ui_arg = LaunchArgument("load_gazebo_ui", False, [True, False])
 world_arg = LaunchArgument("world", "empty_camera.sdf")
 use_realsense_arg = LaunchArgument("realsense", False, [True, False])
@@ -22,20 +21,20 @@ use_realsense_arg = LaunchArgument("realsense", False, [True, False])
 
 def launch_setup(context: LaunchContext) -> None:
     use_sim = use_sim_arg.value(context)
-    start_robot = start_robot_arg.value(context)
-    base_link = base_link_arg.value(context)
+    parent = parent_arg.value(context)
     load_gazebo_ui = load_gazebo_ui_arg.value(context)
     world = str(world_arg.value(context))
     use_realsense = use_realsense_arg.value(context)
 
     namespace = "franka"
     frame_prefix = namespace + "/" if namespace else ""
+    is_mobile_manipulator = parent != "world"
 
     xacro_path = get_file_path("rcdt_franka", ["urdf"], "franka.urdf.xacro")
     xacro_arguments = {}
     xacro_arguments["simulation"] = "true" if use_sim else "false"
     xacro_arguments["namespace"] = namespace
-    xacro_arguments["parent"] = base_link
+    xacro_arguments["parent"] = "" if is_mobile_manipulator else parent
     xacro_arguments["load_realsense"] = "true" if use_realsense else "false"
     robot_description = get_robot_description(xacro_path, xacro_arguments)
 
@@ -43,10 +42,7 @@ def launch_setup(context: LaunchContext) -> None:
         package="robot_state_publisher",
         executable="robot_state_publisher",
         namespace=namespace,
-        parameters=[
-            robot_description,
-            {"frame_prefix": frame_prefix},
-        ],
+        parameters=[robot_description, {"frame_prefix": frame_prefix}],
     )
 
     if use_sim:
@@ -87,9 +83,9 @@ def launch_setup(context: LaunchContext) -> None:
     return [
         SetParameter(name="use_sim_time", value=use_sim),
         robot_state_publisher,
-        robot if start_robot else SKIP,
+        robot if not is_mobile_manipulator else SKIP,
         joint_state_broadcaster,
-        static_transform_publisher if start_robot else SKIP,
+        static_transform_publisher if not is_mobile_manipulator else SKIP,
     ]
 
 
@@ -97,8 +93,7 @@ def generate_launch_description() -> LaunchDescription:
     return LaunchDescription(
         [
             use_sim_arg.declaration,
-            start_robot_arg.declaration,
-            base_link_arg.declaration,
+            parent_arg.declaration,
             load_gazebo_ui_arg.declaration,
             world_arg.declaration,
             use_realsense_arg.declaration,
