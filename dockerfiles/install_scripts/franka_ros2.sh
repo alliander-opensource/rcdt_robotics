698bf9e3d3f845faadeb2eea0678a9a9ce659f88
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-05-13 11:07:21 +0000

    Remove install of ros-humble-dev-tools
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/franka_ros2.sh
+++ dockerfiles/install_scripts/franka_ros2.sh
@@ -8,7 +8,6 @@ set -e
 
 #Install ros2 franka: https://github.com/frankaemika/franka_ros2/tree/v1.0.0
 apt update
-apt install -y ros-humble-dev-tools
 mkdir -p /home/$UNAME/franka_ws/src
 cd /home/$UNAME/franka_ws/src
 git clone -b v1.0.0 https://github.com/frankaemika/franka_ros2.git
