701b81d4566488de479e85f0099816cc5e019a1b
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-05-19 12:05:41 +0000

    Improve register.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_utilities/rcdt_utilities/register.py
+++ ros2_ws/src/rcdt_utilities/rcdt_utilities/register.py
@@ -1,115 +1,165 @@
-import subprocess
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
 
-from launch import LaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit, OnProcessIO
+from typing import Union
+
+from launch import LaunchContext, LaunchDescription
+from launch.actions import (
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit, OnProcessIO, OnProcessStart
 from launch.events.process import ProcessIO
 from launch_ros.actions import Node
-from rclpy import logging
 from termcolor import cprint
 
-logger = logging.get_logger(__name__)
+CONF_NAME = "registered_group"
+
+
+class RegisteredLaunchDescription(IncludeLaunchDescription):
+    """Extends on the default IncludeLaunchDescription, to automatically register an unique group_id."""
+
+    def __init__(self, launch_description_source: str, launch_arguments: dict = None):
+        if launch_arguments is None:
+            launch_arguments = {}
+        self.group_id = Register.get_unique_group_id()
+        launch_arguments[CONF_NAME] = self.group_id
+        super().__init__(
+            launch_description_source,
+            launch_arguments=launch_arguments.items(),
+        )
 
 
 class Register:
-    registered: list["Register"] = []
-    on_all_started_action = False
+    """A class to register the individual processes from launch files, to ensure a desired launch order."""
+
+    group_id = 0
+    register: list[Union["Register", str]] = []
 
     @staticmethod
-    def count_started() -> int:
-        """Returns the number of registered nodes that are started."""
-        n = 0
-        for registered_node in Register.registered:
-            if registered_node.is_started:
-                n += 1
-        return n
+    def get_unique_group_id() -> str:
+        Register.group_id += 1
+        return f"group_{Register.group_id}"
 
     @staticmethod
-    def count_registered() -> int:
-        """Returns the number of registered nodes."""
-        return len(Register.registered)
+    def next(*_: any) -> Node | ExecuteProcess:
+        """
+        Returns the executable of the next registered item that should start.
+
+        This item is located at position 1 of the register list, since position 0 contains the initial register that starts the chain.
+        An item can also be a string representing a group. Therefore we pop until the item at position 1 is of type Register.
+        There are no registered items left to start when the length of the register list equals 1, so then we return en empty launch description.
+        """
+        item = None
+        while not isinstance(item, Register):
+            if len(Register.register) == 1:
+                return LaunchDescription([])
+            item = Register.register.pop(1)
+        log_action_start(item.action)
+        return item.action
 
     @staticmethod
-    def progress() -> str:
-        """Returns a string '[x/y]' with x the started nodes and y the total registered nodes."""
-        return f"[{Register.count_started()}/{len(Register.registered)}]"
+    def group(
+        launch_description: RegisteredLaunchDescription, context: LaunchContext
+    ) -> RegisteredLaunchDescription:
+        """
+        Adds a the group id of the included launch_description to the register.
 
-    @staticmethod
-    def all_started() -> bool:
-        """Returns whether all registered nodes are started."""
-        return Register.count_started() == Register.count_registered()
+        This enables the items of the included launch description to register in the right order later.
+        """
+        name = launch_description.group_id
+        group = context.launch_configurations.get(CONF_NAME)
 
-    def __init__(self, node: Node):
-        self.node = node
+        if group:
+            index = Register.register.index(group)
+            Register.register.insert(index, name)
+        else:
+            Register.register.append(name)
+        return launch_description
+
+    def __init__(self):
+        self.action: Union[Node | ExecuteProcess]
+
         self.is_started = False
-        self.message: str
-        Register.registered.append(self)
+        self.log: str
 
     @classmethod
-    def on_log(cls, node: Node, message: str) -> LaunchDescription:
-        """Registers the given node to be completely started when it's log contains the given message."""
-
-        registered_node = cls(node)
-        registered_node.message = message
+    def on_start(
+        cls, action: Union[Node, ExecuteProcess], context: LaunchContext
+    ) -> LaunchDescription:
+        """Registers the given action to be ready directly on start."""
+        register = cls()
         event_handler = RegisterEventHandler(
-            OnProcessIO(
-                target_action=node,
-                on_stderr=registered_node.process_io,
-            )
+            OnProcessStart(target_action=action, on_start=Register.next)
         )
-        return LaunchDescription([node, event_handler])
+        return register.insert_action(action, event_handler, context)
 
     @classmethod
-    def on_exit(cls, node: Node) -> LaunchDescription:
-        """Registers the given node to be completely started when it exists."""
-        registered_node = cls(node)
+    def on_exit(
+        cls, action: Union[Node, ExecuteProcess], context: LaunchContext
+    ) -> LaunchDescription:
+        """Registers the given action to be ready on exit."""
+        register = cls()
         event_handler = RegisterEventHandler(
-            OnProcessExit(
-                target_action=node, on_exit=lambda *_: registered_node.started()
-            )
+            OnProcessExit(target_action=action, on_exit=Register.next)
         )
-        return LaunchDescription([node, event_handler])
+        return register.insert_action(action, event_handler, context)
 
-    @staticmethod
-    def on_all_started(node: Node) -> LaunchDescription:
-        """
-        Starts a wait_for_topic node that waits for an Empty message on /start topic.
-        This message is published when all registered nodes are started, resulting in the wait_for_topic node to exit.
-        This exit triggers the start of the node passed to this function.
-        """
-        Register.on_all_started_action = True
-
-        wait_for_topic = Node(
-            package="rcdt_utilities",
-            executable="wait_for_topic.py",
-            parameters=[{"topic": "/start"}, {"msg_type": "Empty"}, {"log": False}],
-        )
-
+    @classmethod
+    def on_log(
+        cls, action: Union[Node, ExecuteProcess], log: str, context: LaunchContext
+    ) -> LaunchDescription:
+        """Registers the given action to be ready when logging the given log message."""
+        register = cls()
+        register.log = log
         event_handler = RegisterEventHandler(
-            OnProcessExit(target_action=wait_for_topic, on_exit=node)
+            OnProcessIO(target_action=action, on_stderr=register.process_io)
         )
-        return LaunchDescription([wait_for_topic, event_handler])
+
+        return register.insert_action(action, event_handler, context)
 
     def process_io(self, event: ProcessIO) -> None:
+        """Returns the next register to start if the defined log is captured."""
         if self.is_started:
             return
-        if self.message in event.text.decode():
-            self.started()
+        if self.log in event.text.decode():
+            self.started = True
+            return Register.next()
 
-    def started(self) -> None:
-        self.is_started = True
-        cprint(
-            f"[Registered Nodes] {Register.progress()}: {self.node.name} started successfully!",
-            "blue",
-        )
-        if Register.all_started():
-            self.all_started_action()
+    def insert_action(
+        self,
+        action: Union[Node, ExecuteProcess],
+        event_handler: RegisterEventHandler,
+        context: LaunchContext,
+    ) -> LaunchDescription:
+        """
+        Inserts the action in the register and returns a launch description.
 
-    def all_started_action(self) -> None:
-        cprint("[Registered Nodes] : All nodes started!", "blue")
-        if Register.on_all_started_action:
-            subprocess.run(
-                ["ros2", "topic", "pub", "--once", "/start", "std_msgs/msg/Empty"],
-                check=False,
-                stdout=subprocess.DEVNULL,
-            )
+        If a group is defined, the index is defined based on the index of the group.
+        Otherwise, the index is simply the length of the register, so that the item is appended at the end.
+
+        A launch description is returned with the event_handler.
+        If the item is the first action to start, the launch_description also contains the action to trigger the chain reaction.
+        """
+        self.action = action
+        group = context.launch_configurations.get(CONF_NAME)
+        index = Register.register.index(group) if group else len(Register.register)
+        Register.register.insert(index, self)
+
+        if index == 0:
+            log_action_start(action)
+            return LaunchDescription([action, event_handler])
+        else:
+            return LaunchDescription([event_handler])
+
+
+def log_action_start(action: Union[Node, ExecuteProcess]) -> None:
+    if isinstance(action, Node):
+        name = "[node]: " + action.node_executable
+    else:
+        name = "[process]: "
+        for part in action.cmd:
+            name += part[0].text + " "
+    cprint(f"starting: {name}", "blue")
