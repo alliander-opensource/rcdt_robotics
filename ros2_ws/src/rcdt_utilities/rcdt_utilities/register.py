efd3260a511ba95396710564d630a428e64ed864
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-05-27 12:07:41 +0000

    Use Any as type.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- ros2_ws/src/rcdt_utilities/rcdt_utilities/register.py
+++ ros2_ws/src/rcdt_utilities/rcdt_utilities/register.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Union
+from typing import Any, Union
 
 from launch import LaunchContext, LaunchDescription
 from launch.actions import (
@@ -89,11 +89,11 @@ class Register:
         Register.started = 0
 
     @staticmethod
-    def next(*_: RegisteredLaunchDescription) -> LaunchDescription:
+    def next(*_: Any) -> LaunchDescription:
         """Returns the next action to start based on the order of registration.
 
         Args:
-            *_ (RegisteredLaunchDescription): Ignored.  Comes from the OpaqueFunction.
+            *_ (Any): Ignored. Additional arguments from `OnProcessStart` or `OnProcessExit`.
 
         Returns:
             LaunchDescription: A launch description containing the next action to start, or an empty launch description if all actions have been started.
