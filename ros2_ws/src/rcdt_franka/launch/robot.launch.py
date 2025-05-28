600ee961542270ff1eedc0e623b7a8dfb1da22fb
Max Waterhout <max.waterhout@soprasteria.com>
2025-05-28 12:46:46 +0000

    Add environment variable support and refactor launch setup for franka robot
    Signed-off-by: Max Waterhout <max.waterhout@soprasteria.com>

--- ros2_ws/src/rcdt_franka/launch/robot.launch.py
+++ ros2_ws/src/rcdt_franka/launch/robot.launch.py
@@ -6,50 +6,87 @@ from launch import LaunchDescription
 from launch.actions import Shutdown
 from launch_ros.actions import Node
 from rcdt_utilities.launch_utils import get_file_path
+import os 
+from launch import LaunchContext, LaunchDescription
+from rcdt_utilities.register import Register, RegisteredLaunchDescription
+from launch.actions import OpaqueFunction
 
-namespace = "franka"
-ns = f"/{namespace}" if namespace else ""
 
-franka_controllers = param_file = get_file_path(
-    "rcdt_franka", ["config"], "controllers.yaml"
-)
+def launch_setup(context: LaunchContext):
+    namespace = "franka"
+    ns = f"/{namespace}" if namespace else ""
 
-ros2_control_node = Node(
-    package="controller_manager",
-    executable="ros2_control_node",
-    parameters=[
-        franka_controllers,
-        {"arm_id": "fr3"},
-    ],
-    remappings=[
-        (f"{ns}/controller_manager/robot_description", f"{ns}/robot_description"),
-        (f"{ns}/joint_states", f"{ns}/fr3_arm/joint_states"),
-    ],
-    namespace=namespace,
-    on_exit=Shutdown(),
-)
 
-settings_setter = Node(
-    package="rcdt_franka",
-    executable="settings_setter.py",
-)
+    hostname = os.getenv('FRANKA_HOSTNAME', '')
+    username = os.getenv('FRANKA_USERNAME', 'admin')
+    password = os.getenv('FRANKA_PASSWORD', '')
 
-joint_state_publisher = Node(
-    package="joint_state_publisher",
-    executable="joint_state_publisher",
-    name="joint_state_publisher",
-    parameters=[
-        {
-            "source_list": [
-                f"{ns}/fr3_arm/joint_states",
-                f"{ns}/fr3_gripper/joint_states",
-            ],
-            "rate": 30,
-        }
-    ],
-    namespace=namespace,
-)
+    if not hostname or not username or not password:
+        raise RuntimeError(
+            "You must set FRANKA_HOSTNAME and FRANKA_PASSWORD in your environment."
+        )
 
+    # only include the node if we actually have a hostname and password
+    franka_lock_unlock = Node(
+        name="franka_lock_unlock",
+        package="franka_lock_unlock",
+        executable="franka_lock_unlock.py",
+        output='screen',
+        arguments=[
+            hostname,
+            username,
+            password,
+            '-u','-l', '-w','-r',  '-p','-c'            
+        ],
+        respawn=True
+    )
+
+    franka_controllers = param_file = get_file_path(
+        "rcdt_franka", ["config"], "controllers.yaml"
+    )
+
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            franka_controllers,
+            {"arm_id": "fr3"},
+        ],
+        remappings=[
+            (f"{ns}/controller_manager/robot_description", f"{ns}/robot_description"),
+            (f"{ns}/joint_states", f"{ns}/fr3_arm/joint_states"),
+        ],
+        namespace=namespace,
+        on_exit=Shutdown(),
+    )
+
+    settings_setter = Node(
+        package="rcdt_franka",
+        executable="settings_setter.py",
+    )
+
+    joint_state_publisher = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        parameters=[
+            {
+                "source_list": [
+                    f"{ns}/fr3_arm/joint_states",
+                    f"{ns}/fr3_gripper/joint_states",
+                ],
+                "rate": 30,
+            }
+        ],
+        namespace=namespace,
+    )
+    
+    return[
+        Register.on_log(franka_lock_unlock,"Keeping persistent connection...", context),
+        Register.on_start(settings_setter, context),
+        Register.on_start(ros2_control_node, context),
+        Register.on_start(joint_state_publisher, context) 
+    ]
 
 def generate_launch_description() -> LaunchDescription:
     """Generate the launch description for the Franka robot controllers.
@@ -59,8 +96,7 @@ def generate_launch_description() -> LaunchDescription:
     """
     return LaunchDescription(
         [
-            settings_setter,
-            ros2_control_node,
-            joint_state_publisher,
+            OpaqueFunction(function=launch_setup),
+
         ]
     )
