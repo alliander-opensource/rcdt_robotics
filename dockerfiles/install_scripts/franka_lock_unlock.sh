4e0c0c83d307495c91410386a8ea747fc82d10ac
Max Waterhout <max.waterhout@soprasteria.com>
2025-05-28 11:15:27 +0200

    Add franka_lock_unlock installation script to Dockerfile
    Signed-off-by: Max Waterhout <max.waterhout@soprasteria.com>

---
+++ dockerfiles/install_scripts/franka_lock_unlock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -i
+
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+mkdir -p /home/$UNAME/franka_lock_unlock/src
+cd /home/$UNAME/franka_lock_unlock
+git clone https://github.com/jk-ethz/franka_lock_unlock.git
+
+
+rosdep update
+rosdep install --from-paths src --rosdistro humble -y -i
+
+source /home/$UNAME/.bashrc
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+echo "source /home/$UNAME/franka_lock_unlock/install/setup.bash" >>/home/$UNAME/.bashrc
