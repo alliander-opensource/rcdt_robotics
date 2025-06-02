0da4344963e19ac276bce766b314a90a96deb8c2
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-06-02 11:32:36 +0000

    Set version to 2.2.1
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/husarion_ugv_ros.sh
+++ dockerfiles/install_scripts/husarion_ugv_ros.sh
@@ -11,7 +11,7 @@ apt update
 cd /home/$UNAME
 mkdir husarion_ws
 cd /home/$UNAME/husarion_ws
-git clone -b 2.1.2 https://github.com/husarion/husarion_ugv_ros.git src/husarion_ugv_ros
+git clone -b 2.2.1 https://github.com/husarion/husarion_ugv_ros.git src/husarion_ugv_ros
 export HUSARION_ROS_BUILD_TYPE=simulation
 vcs import src < src/husarion_ugv_ros/husarion_ugv/${HUSARION_ROS_BUILD_TYPE}_deps.repos
 
