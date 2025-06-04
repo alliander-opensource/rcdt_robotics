6ccc165a9fe327b710d6ec72223bed62b0aad3b1
Jelmer de Wolde <Jelmerdw@users.noreply.github.com>
2025-06-04 15:39:54 +0200

    Install franka_ros2 v1.0.2 in docker. (#191)
    * Install franka_ros2 v1.0.2 in docker.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Make script executable.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    * Remove temporary gpg fix.
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>
    
    ---------
    
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/franka_ros2.sh
+++ dockerfiles/install_scripts/franka_ros2.sh
@@ -6,14 +6,13 @@
 
 set -e
 
-#Install ros2 franka: https://github.com/frankaemika/franka_ros2/tree/v1.0.0
+#Install ros2 franka: https://github.com/frankaemika/franka_ros2/tree/v1.0.2
 apt update
-mkdir -p /home/$UNAME/franka_ws/src
-cd /home/$UNAME/franka_ws/src
-git clone -b v1.0.0 https://github.com/frankaemika/franka_ros2.git
-git clone -b 0.4.0 https://github.com/frankaemika/franka_description.git
-cd /home/$UNAME/franka_ws
-rosdep install --from-paths src --ignore-src --rosdistro humble -y
 source /home/$UNAME/.bashrc
+mkdir -p /home/$UNAME/franka_ws/src
+cd /home/$UNAME/franka_ws
+git clone -b v1.0.2 https://github.com/frankaemika/franka_ros2.git src
+vcs import src < src/franka.repos --recursive --skip-existing
+rosdep install --from-paths src --ignore-src --rosdistro humble -y
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-ignore franka_ign_ros2_control
 echo "source /home/$UNAME/franka_ws/install/setup.bash" >>/home/$UNAME/.bashrc
