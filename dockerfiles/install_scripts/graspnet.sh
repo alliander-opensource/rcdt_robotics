3ef82142acecd7f0e983f09be9627f7f53525f90
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-06-10 10:09:00 +0000

    Rename.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/graspnet.sh
+++ dockerfiles/install_scripts/graspnet.sh
@@ -8,7 +8,9 @@ set -e
 
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/#debian:
 
+apt update
+apt install -y wget
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
-apt-get update
-apt install cuda-toolkit
+apt update
+apt install -y cuda-toolkit
