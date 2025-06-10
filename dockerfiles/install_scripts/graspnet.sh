e59a709564d3814686d1d844fd9449b1f5c63cbc
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-06-10 12:45:08 +0000

    Switch to our graspnet fork.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/graspnet.sh
+++ dockerfiles/install_scripts/graspnet.sh
@@ -6,8 +6,7 @@
 
 set -e
 
-# https://docs.nvidia.com/cuda/cuda-installation-guide-linux/#debian:
-
+# Install cuda: https://docs.nvidia.com/cuda/cuda-installation-guide-linux/#debian
 apt update
 apt install -y wget
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
@@ -15,4 +14,6 @@ dpkg -i cuda-keyring_1.1-1_all.deb
 apt update
 apt install -y cuda-toolkit
 
-git clone https://github.com/graspnet/graspnet-baseline.git
+# Clone our graspnet fork:
+cd /home/$UNAME
+git clone https://github.com/alliander-opensource/graspnet-baseline.git
