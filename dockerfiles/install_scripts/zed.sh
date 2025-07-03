b75ea77440e7da3ac04b7f09343871b10c035567
Max Waterhout <max.waterhout@hotmail.nl>
2025-07-03 14:17:11 +0000

    Remove execution of ZED SDK installer and cleanup commands from installation script
    Signed-off-by: Max Waterhout <max.waterhout@hotmail.nl>

--- dockerfiles/install_scripts/zed.sh
+++ dockerfiles/install_scripts/zed.sh
@@ -17,6 +17,4 @@ ENV NVIDIA_DRIVER_CAPABILITIES \
 # Setup the ZED SDK
 RUN apt-get update -y || true ; apt-get install --no-install-recommends wget zstd udev libgomp1 -y ; \
     wget -O ZED_SDK_Linux_Ubuntu${UBUNTU_RELEASE_YEAR}.run https://download.stereolabs.com/zedsdk/${ZED_SDK_MAJOR}.${ZED_SDK_MINOR}/cu${CUDA_MAJOR}/ubuntu${UBUNTU_RELEASE_YEAR} && \
-    chmod +x ZED_SDK_Linux_Ubuntu${UBUNTU_RELEASE_YEAR}.run ; ./ZED_SDK_Linux_Ubuntu${UBUNTU_RELEASE_YEAR}.run -- silent runtime_only skip_cuda && \
-    rm ZED_SDK_Linux_Ubuntu${UBUNTU_RELEASE_YEAR}.run && \
-    rm -rf /var/lib/apt/lists/*
+    chmod +x ZED_SDK_Linux_Ubuntu${UBUNTU_RELEASE_YEAR}.run 
