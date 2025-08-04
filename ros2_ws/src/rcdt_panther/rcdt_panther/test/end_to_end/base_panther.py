360c90e9323246945aeb91ae3bc55cb8ac31325e
Max Waterhout <max.waterhout@hotmail.nl>
2025-08-04 06:20:54 +0000

    WIP
    Signed-off-by: Max Waterhout <max.waterhout@hotmail.nl>

--- ros2_ws/src/rcdt_panther/rcdt_panther/test/end_to_end/base_panther.py
+++ ros2_ws/src/rcdt_panther/rcdt_panther/test/end_to_end/base_panther.py
@@ -124,6 +124,7 @@ def get_tests() -> dict:
             frame_base="panther/base_link",
             frame_target="panther/odom",
             timeout=timeout,
+            publish_duration=3.0,
         )
 
     def test_e_stop_request_trigger(
