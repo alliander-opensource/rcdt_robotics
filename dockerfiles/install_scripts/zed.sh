e118aaf57badc711b912e87205d60e3c10d3170e
Max Waterhout <max.waterhout@hotmail.nl>
2025-08-05 10:28:05 +0000

    WIP
    Signed-off-by: Max Waterhout <max.waterhout@hotmail.nl>

---
+++ dockerfiles/install_scripts/zed.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -i
+
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+apt update
+
+UBUNTU_RELEASE_YEAR=22    
+CUDA_MAJOR=11             
+ZED_SDK_MAJOR=5
+ZED_SDK_MINOR=0
+
+sudo apt-get update -y
+sudo apt-get install --no-install-recommends -y wget zstd udev libgomp1
+
+wget -q --content-disposition --trust-server-names \
+     "https://download.stereolabs.com/zedsdk/${ZED_SDK_MAJOR}.${ZED_SDK_MINOR}/cu${CUDA_MAJOR}/ubuntu${UBUNTU_RELEASE_YEAR}"
+
+
+installer=$(ls ZED_SDK*run)
+
+sudo chmod +x "$installer"
+
+sudo "./$installer" silent skip_tools skip_cuda
+
+rm "$installer"
+
+cd /home/$UNAME
+mkdir zed_ws
+cd /home/$UNAME/zed_ws
+git clone https://github.com/stereolabs/zed-ros2-wrapper.git src/zed_ros2_wrapper
+
+rosdep install --from-paths src --ignore-src -r -y # install dependencies
+source /home/$UNAME/.bashrc
+
+colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release
+echo "source /home/$UNAME/zed_ws/install/setup.bash" >>/home/$UNAME/.bashrc
+
+ln -sf /lib/x86_64-linux-gnu/libusb-1.0.so.0 /usr/lib/x86_64-linux-gnu/libusb-1.0.so
