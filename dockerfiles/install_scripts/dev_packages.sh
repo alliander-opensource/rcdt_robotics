edcba50ff41c04ff5dcf14dee7e09de15b78a925
Max Waterhout <max.waterhout@hotmail.nl>
2025-08-22 06:55:26 +0000

    Remove unnecessary installation steps from dev_packages.sh
    Signed-off-by: Max Waterhout <max.waterhout@hotmail.nl>

--- dockerfiles/install_scripts/dev_packages.sh
+++ dockerfiles/install_scripts/dev_packages.sh
@@ -7,18 +7,3 @@
 set -e
 source /home/rcdt/.bashrc
 apt update
-
-apt install ros-jazzy-pointcloud-to-laserscan
-
-sudo apt-get install -y libpcap-dev
-
-mkdir -p /home/rcdt/velodyne_ws/src
-cd /home/rcdt/velodyne_ws
-git clone -b ros2 https://github.com/alliander-opensource/velodyne.git
-rosdep update --rosdistro jazzy
-rosdep install --from-paths src --ignore-src --rosdistro jazzy -y
-
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
-echo "source /home/rcdt/velodyne_ws/install/setup.bash" >>/home/rcdt/.bashrc
-
-source /home/rcdt/.bashrc
