881bcb1ebd81dc1a164a68724d9a88d5941b1320
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-08-27 07:53:54 +0000

    Add vizanti to docker.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

---
+++ dockerfiles/install_scripts/vizanti.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -i
+
+# SPDX-FileCopyrightText: Alliander N. V.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+source /home/$UNAME/.bashrc
+apt update
+
+mkdir -p /home/$UNAME/vizanti_ws/src
+cd /home/$UNAME/vizanti_ws/src
+git clone -b ros2 https://github.com/MoffKalast/vizanti.git
+
+cd /home/$UNAME/vizanti_ws
+rosdep update --rosdistro $ROS_DISTRO
+rosdep install --from-paths src -y -i
+
+colcon build --symlink-install
+echo "source /home/$UNAME/vizanti_ws/install/setup.bash" >>/home/$UNAME/.bashrc
