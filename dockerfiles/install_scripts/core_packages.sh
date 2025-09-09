f13d62fc16457de5c086060a13b2024a9cd1dd87
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-09-09 10:32:55 +0000

    Switch to pointcloud-to-laserscan node.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/core_packages.sh
+++ dockerfiles/install_scripts/core_packages.sh
@@ -31,8 +31,7 @@ apt install -y \
     ros-$ROS_DISTRO-ros2-controllers \
     ros-$ROS_DISTRO-rqt-tf-tree \
     ros-$ROS_DISTRO-slam-toolbox \
-    ros-$ROS_DISTRO-velodyne \
-    ros-$ROS_DISTRO-velodyne-description
+    ros-$ROS_DISTRO-pointcloud-to-laserscan
 
 pip install uv --break-system-packages
 
