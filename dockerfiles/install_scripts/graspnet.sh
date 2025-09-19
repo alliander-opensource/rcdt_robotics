0b379c9784fca899b40e80677b456cd15d5c4ec7
Max Waterhout <max.waterhout@hotmail.nl>
2025-09-19 12:15:02 +0200

    remove cuda from container
    Signed-off-by: Max Waterhout <max.waterhout@hotmail.nl>

--- dockerfiles/install_scripts/graspnet.sh
+++ dockerfiles/install_scripts/graspnet.sh
@@ -5,14 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
-
-# Install cuda: https://docs.nvidia.com/cuda/cuda-installation-guide-linux/#debian
+source /home/$UNAME/.bashrc
 apt update
-apt install -y wget
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda-keyring_1.1-1_all.deb
-apt update
-apt install -y cuda-toolkit
 
 # Clone our graspnet fork:
 cd /home/$UNAME
