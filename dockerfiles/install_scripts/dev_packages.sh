d577008fcaea2d918e1d0ebf34bb9da7b202d9ee
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-10-28 15:22:22 +0000

    Try to run tests with xvfb in runner.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- dockerfiles/install_scripts/dev_packages.sh
+++ dockerfiles/install_scripts/dev_packages.sh
@@ -11,6 +11,7 @@ apt update
 apt install -y \
     clang \
     doxygen \
+    xvfb \
     ros-$ROS_DISTRO-nmea-navsat-driver \
     ros-$ROS_DISTRO-moveit-ros-perception \
     ros-$ROS_DISTRO-topic-tools
