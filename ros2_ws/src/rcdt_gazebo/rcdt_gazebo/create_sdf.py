d46dfab258e079b82dfcefb3b75c3f7d54b90daa
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2025-11-11 11:58:44 +0000

    Create 3D world form open street map.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

---
+++ ros2_ws/src/rcdt_gazebo/rcdt_gazebo/create_sdf.py
@@ -0,0 +1,92 @@
+import subprocess
+
+import xmltodict
+from pyproj import Geod
+from rcdt_utilities.launch_utils import get_file_path
+from termcolor import colored
+
+
+def create_map_world(lon: float, lat: float) -> None:
+    """Create a map world SDF file based on the given longitude and latitude.
+
+    Args:
+        lon (float): The longitude of the map center.
+        lat (float): The latitude of the map center.
+    """
+    download_map(lon, lat)
+    convert_map()
+    create_sfd(lon, lat)
+
+
+def download_map(lon: float, lat: float) -> None:
+    """Download map data from OpenStreetMap for the specified longitude and latitude.
+
+    Args:
+        lon (float): The longitude of the map center.
+        lat (float): The latitude of the map center.
+    """
+    print(colored("Downloading map data from OpenStreetMap...", "yellow"))
+    geo = Geod(ellps="WGS84")
+    distance = 100  # meters
+
+    lats = []
+    longs = []
+    angle = 45
+
+    for _ in range(4):
+        corner = geo.fwd(lon, lat, angle, distance)
+        longs.append(corner[0])
+        lats.append(corner[1])
+        angle += 90
+
+    min_lon = min(longs)
+    max_lon = max(longs)
+    min_lat = min(lats)
+    max_lat = max(lats)
+
+    cmd_download = [
+        "wget",
+        "-O",
+        "/tmp/map.osm",
+        f"https://api.openstreetmap.org/api/0.6/map?bbox={min_lon},{min_lat},{max_lon},{max_lat}",
+    ]
+    subprocess.run(
+        cmd_download, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+
+
+def convert_map() -> None:
+    """Convert the downloaded OSM map data to glTF format."""
+    print(colored("Converting map data to glTF format...", "yellow"))
+    cmd_convert = [
+        "bash",
+        "/home/rcdt/osm2world/osm2world.sh",
+        "convert",
+        "-i",
+        "/tmp/map.osm",
+        "-o",
+        "/tmp/map.gltf",
+    ]
+    subprocess.run(
+        cmd_convert, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+
+
+def create_sfd(lon: float, lat: float) -> None:
+    """Create an SDF world file from the glTF map object.
+
+    Args:
+        lon (float): The longitude of the map center.
+        lat (float): The latitude of the map center.
+    """
+    print(colored("Creating SDF file from glTF object...", "yellow"))
+    file_path = get_file_path("rcdt_gazebo", ["worlds"], "world.sdf")
+    with open(file_path, encoding="utf-8") as fd:
+        sdf_string = fd.read()
+
+    sdf_dict = xmltodict.parse(sdf_string)
+    sdf_dict["sdf"]["world"]["spherical_coordinates"]["longitude_deg"] = lon
+    sdf_dict["sdf"]["world"]["spherical_coordinates"]["latitude_deg"] = lat
+
+    with open("/tmp/world.sdf", "w", encoding="utf-8") as fd:
+        fd.write(xmltodict.unparse(sdf_dict))
--- ros2_ws/src/rcdt_launch/launch/robots.launch.py
+++ ros2_ws/src/rcdt_launch/launch/robots.launch.py
@@ -115,7 +115,7 @@ def launch_setup(context: LaunchContext) -> list:  # noqa: PLR0912, PLR0915
             panther = Vehicle("panther", [0, 0, 0.2])
             Camera("realsense", [0, 0, 0.2], parent=panther)
         case "panther_gps":
-            Platform.world = "empty_camera.sdf"
+            Platform.world = "map_5.940906_51.966960"
             use_vizanti = True
             panther = Vehicle("panther", [0, 0, 0.2], navigation=True, gps=True)
             Lidar("velodyne", [0.13, -0.13, 0.35], parent=panther)
